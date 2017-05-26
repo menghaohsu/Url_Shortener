bae3df9431fd6f0280664938b8693320f22eebe6
menghaohsu <hsu.me@husky.neu.edu>
2017-05-25 20:29:00 -0400

    fix bug

--- server/db/model/url.js
+++ server/db/model/url.js
@@ -38,25 +38,3 @@ module.exports = db.define('url',{
 		}
 	}
 });
-
-
-const ust = () => {
-	
-
-	this.encode = (num) => {
-		let shortenUrl = '';
-		while(num>0){
-			shortenUrl = char.charAt(num%length) + shortenUrl;
-			num = Math.floor(num/length);
-		}
-		return shortenUrl;
-	};
-
-	this.decode = (str) => {
-		let num = 0;
-		for(let i=0; i<str.length; i+=1){
-			num = num*length +char.indexOf(str.charAt(i));
-		}
-		return num;
-	}
-};
