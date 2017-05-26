4d8c7bea8e270bd05bb54cd13b008f3eb01c6051
menghaohsu <hsu.me@husky.neu.edu>
2017-05-25 20:28:05 -0400

    finished basic frontend, encode decode method

--- server/db/model/url.js
+++ server/db/model/url.js
@@ -1,6 +1,9 @@
 const Sequelize = require('sequelize');
 const db = require('../_db');
 
+const char = '1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ-_';
+const length = char.length;
+
 module.exports = db.define('url',{
 	url: {
 		type: Sequelize.STRING
@@ -8,4 +11,52 @@ module.exports = db.define('url',{
 	shortenUrl: {
 		type: Sequelize.STRING
 	}
+},{
+	classMethods: {
+		encode: (num) => {
+			let shortenUrl = '';
+			while(num>0){
+				shortenUrl = char.charAt(num%length) + shortenUrl;
+				num = Math.floor(num/length);
+			}
+			return shortenUrl;
+		}
+	},
+	instanceMethods: {
+		decode: (str) => {
+			let num = 0;
+			for(let i=0; i<str.length; i+=1){
+				num = num*length +char.indexOf(str.charAt(i));
+			}
+			return num;
+		}
+	},
+	hooks: {
+		afterCreate: (url) => {
+			let sUrl = url.Model.encode(url.id);
+			return url.updateAttributes({ shortenUrl: sUrl});
+		}
+	}
 });
+
+
+const ust = () => {
+	
+
+	this.encode = (num) => {
+		let shortenUrl = '';
+		while(num>0){
+			shortenUrl = char.charAt(num%length) + shortenUrl;
+			num = Math.floor(num/length);
+		}
+		return shortenUrl;
+	};
+
+	this.decode = (str) => {
+		let num = 0;
+		for(let i=0; i<str.length; i+=1){
+			num = num*length +char.indexOf(str.charAt(i));
+		}
+		return num;
+	}
+};
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,11 +1,47 @@
 const express = require('express');
-const router = express.Router();
+const Url = require('../db/model/url')
 const path = require('path');
+
+const router = express.Router();
 
 module.exports = () => {
 
-	router.get('/', function (req, res, next) {
-    res.sendFile(path.join(__dirname, '../views/index.html'))
+	router.get('/', (req, res, next) => {
+    res.sendFile(path.join(__dirname, '../views/index.html'), (err)=>{
+    	if(err) next(err);
+    })
   });
+
+  router.post('/', (req, res, next) => {
+  	Url.create(req.body)
+  	.then((url) => {
+  		res.status(200).send(url)
+  	})
+  	.catch(next);
+  });
+
 	return router;
 }
+
+const urlShortener = () => {
+	const char = '1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ-_';
+	const length = char.length;
+
+	this.encode = (num) => {
+		let shortenUrl = '';
+		while(num>0){
+			shortenUrl = char.charAt(num%length) + shortenUrl;
+			num = Math.floor(num/length);
+		}
+		return shortenUrl;
+	};
+
+	this.decode = (str) => {
+		let num = 0;
+		for(let i=0; i<str.length; i+=1){
+			num = num*length +char.indexOf(str.charAt(i));
+		}
+		return num;
+	}
+};
+
